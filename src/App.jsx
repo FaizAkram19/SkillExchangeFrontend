7717dafbf752f4757e7409c9c916000b86204a10
Faiz Akram <172576315+FaizAkram19@users.noreply.github.com>
2026-05-14 17:12:01 +0000

    Added protected routes that ensures authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
+import ProtectedRoute from './components/ProtectedRoute'
 import Login from './pages/Login'
 import Register from './pages/Register'
 
@@ -11,9 +12,11 @@ function App() {
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<div>Dashboard</div>} />
-          <Route path="/profile" element={<div>Profile</div>} />
-          <Route path="/connections" element={<div>Connections</div>} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/dashboard" element={<div>Dashboard</div>} />
+            <Route path="/profile" element={<div>Profile</div>} />
+            <Route path="/connections" element={<div>Connections</div>} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </AuthProvider>
